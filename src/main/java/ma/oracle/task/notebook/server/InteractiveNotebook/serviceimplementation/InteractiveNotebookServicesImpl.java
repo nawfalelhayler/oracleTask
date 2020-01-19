c7b7b3a7aa10eb2ef174726ce9b43301b5a43aea
Nawfal <nawfalelhaymer@gmail.com>
2020-01-19 18:41:14 +0100

    adding the class InteractiveNotebookServicesImpl that contains the implementation of the services

--- src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/serviceimplementation/InteractiveNotebookServicesImpl.java
+++ src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/serviceimplementation/InteractiveNotebookServicesImpl.java
@@ -1,7 +1,11 @@
 package ma.oracle.task.notebook.server.InteractiveNotebook.serviceimplementation;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ma.oracle.task.notebook.server.InteractiveNotebook.common.CommonServices;
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.requestmodels.InterpreterRequestModel;
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.responsemodels.InterpreterResponseModel;
 import ma.oracle.task.notebook.server.InteractiveNotebook.service.InteractiveNotebookServices;
@@ -9,9 +13,24 @@ import ma.oracle.task.notebook.server.InteractiveNotebook.service.InteractiveNot
 @Service
 public class InteractiveNotebookServicesImpl implements InteractiveNotebookServices {
 
-	@Override
-	public InterpreterResponseModel executecommand(InterpreterRequestModel interpreterrequestmodel) {
-		return null;
+	InterpreterResponseModel interpreterresponsemodel;
+	CommonServices commonservices;
+
+	@Autowired
+	public InteractiveNotebookServicesImpl(InterpreterResponseModel interpreterresponsemodel,
+			CommonServices commonservice) {
+		this.interpreterresponsemodel = interpreterresponsemodel;
+		this.commonservices = commonservice;
 	}
 
+	@Override
+	public InterpreterResponseModel executecommand(InterpreterRequestModel interpreterrequestmodel)
+			throws InterruptedException, IOException {
+		commonservices.commandRegex("\\s(.*)", interpreterrequestmodel);
+		commonservices.createScript("D:\\Microworkspace\\InteractiveNotebook\\script.py", interpreterrequestmodel);
+		String resultat = commonservices.executeScript("D:\\Microworkspace\\InteractiveNotebook\\script.py");
+		interpreterresponsemodel.setResult(resultat);
+		return interpreterresponsemodel;
+
+	}
 }
