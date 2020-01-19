a90c119b84e54df8d71accc8932ebd3a12cbafc7
Nawfal <nawfalelhaymer@gmail.com>
2020-01-19 18:20:02 +0100

    adding a common package that contains three functions : executeScript,createScript,commandRegex

---
+++ src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/common/CommonServices.java
@@ -0,0 +1,79 @@
+package ma.oracle.task.notebook.server.InteractiveNotebook.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.*;
+
+import ma.oracle.task.notebook.server.InteractiveNotebook.models.requestmodels.InterpreterRequestModel;
+import ma.oracle.task.notebook.server.InteractiveNotebook.models.responsemodels.InterpreterResponseModel;
+
+@Component
+public class CommonServices {
+
+	private static final Logger LOGGER = Logger.getLogger(CommonServices.class.getName());
+	InterpreterResponseModel interpreterresponsemodel;
+
+	@Autowired
+	public CommonServices(InterpreterResponseModel interpreterresponsemodel) {
+		this.interpreterresponsemodel = interpreterresponsemodel;
+	}
+
+	/*
+	 * Function that takes the input and retrieve the command using Regex, the goal
+	 * is to have the command without %,interpreter name and the white space
+	 */
+	public String commandRegex(String theRegex, InterpreterRequestModel code) {
+		String resultRegex = null;
+		String command = code.getCode();
+		Pattern checkRegex = Pattern.compile(theRegex);
+		Matcher regexMatcher = checkRegex.matcher(command);
+		while (regexMatcher.find()) {
+			if (regexMatcher.group().length() != 0) {
+				resultRegex = regexMatcher.group().replaceAll("^\\s+", "");
+			}
+		}
+		return resultRegex;
+	}
+
+	/*
+	 * Function to create a script file base on the command thats will be sent to
+	 * the application
+	 */
+	public void createScript(String scriptLocation, InterpreterResponseModel interpreterresponsemodel)
+			throws IOException {
+		try (FileWriter myWriter = new FileWriter(scriptLocation)) {
+			myWriter.write(interpreterresponsemodel.getResult().replaceAll("^\\s+", ""));
+			LOGGER.log(Level.FINE, "Successfully wrote to the file.");
+		}
+	}
+
+	/*
+	 * Function that excute the script sent by a user,the process of the interpreter
+	 * should be running and aded to the path
+	 */
+	public String executeScript(String script) throws IOException, InterruptedException {
+		Process p = Runtime.getRuntime().exec("python " + script);
+		String line;
+		p.waitFor();
+		BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		error.close();
+		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		line = input.readLine();
+		LOGGER.log(Level.FINE, "the response is: {0}", line);
+		OutputStream outputStream = p.getOutputStream();
+		PrintStream printStream = new PrintStream(outputStream);
+		printStream.println();
+		printStream.flush();
+		printStream.close();
+		return line;
+	}
+}
