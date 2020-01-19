880ad0c528eda353870540eafe73e43204a5b7b1
Nawfal <nawfalelhaymer@gmail.com>
2020-01-19 18:42:29 +0100

    add the controller with the endpoint '/execute'

--- src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/Controller/Controller.java
+++ src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/Controller/Controller.java
@@ -1,5 +1,8 @@
 package ma.oracle.task.notebook.server.InteractiveNotebook.Controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.requestmodels.InterpreterRequestModel;
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.responsemodels.InterpreterResponseModel;
+import ma.oracle.task.notebook.server.InteractiveNotebook.service.InteractiveNotebookServices;
 
 @RestController
 @RequestMapping("/interpreter")
 public class Controller {
-	
-	@PostMapping
-	private ResponseEntity<InterpreterResponseModel> executecommand(@RequestBody InterpreterRequestModel command){
-		InterpreterResponseModel interpreterresponsemodel = null;
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(interpreterresponsemodel);
+
+	@Autowired
+	InteractiveNotebookServices interactivenotebookservices;
+
+	@PostMapping("/execute")
+	public ResponseEntity<InterpreterResponseModel> executecommand(@RequestBody InterpreterRequestModel command)
+			throws InterruptedException, IOException {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(interactivenotebookservices.executecommand(command));
 	}
 
 }
