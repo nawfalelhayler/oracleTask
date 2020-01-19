d2f3079b1ba78994416c16808cb69339118be2fa
Nawfal <nawfalelhaymer@gmail.com>
2020-01-19 18:40:24 +0100

    the interface that containes the signature of the service endpoint

--- src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/service/InteractiveNotebookServices.java
+++ src/main/java/ma/oracle/task/notebook/server/InteractiveNotebook/service/InteractiveNotebookServices.java
@@ -1,10 +1,12 @@
 package ma.oracle.task.notebook.server.InteractiveNotebook.service;
 
+import java.io.IOException;
+
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.requestmodels.InterpreterRequestModel;
 import ma.oracle.task.notebook.server.InteractiveNotebook.models.responsemodels.InterpreterResponseModel;
 
 public interface InteractiveNotebookServices {
 
-	InterpreterResponseModel executecommand(InterpreterRequestModel interpreterrequestmodel);
+	InterpreterResponseModel executecommand(InterpreterRequestModel interpreterrequestmodel) throws InterruptedException, IOException;
 
 }
